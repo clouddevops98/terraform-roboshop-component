63f137c6bd5c5d16d91e99382c5ede7420ea04d6
somayya <somayya.adusumalli98@gmail.com>
2025-11-13 18:56:31 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   ami_id = data.aws_ami.joindevops.id
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-  listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : backend_alb_listener_arn
+  listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
   host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}" 
 
   common_tags = {
